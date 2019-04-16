398e95dc653efd42d5a32a0586d3ca84d235afe2
Mario Noseda <mnoseda@bluewin.ch>
2019-04-16 21:43:36 +0200

    initialized project

---
+++ frackstock/main.c
@@ -0,0 +1,18 @@
+/*
+ * frackstock.c
+ *
+ * Created: 16/04/2019 21:42:56
+ * Author : Mario
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
