f8d186aadffcb41bbaff81e93693673f416022db
Mario Noseda <mnoseda@bluewin.ch>
2019-04-17 22:41:07 +0200

    initialized the sensor. reading x-axis

--- frackstock/main.c
+++ frackstock/main.c
@@ -21,8 +21,43 @@
 #define MIN_RAMP_BOTTOM 40
 #define MAX_RAMP_BOTTOM 70
 
-#define FLICKER_TEST
+//#define FLICKER_TEST
+#define ACCEL_TEST
 
+#define ADXL345_ADDRESS 0x53
+/* ADXL345/6 Register Map */
+#define DEVID		0x00	/* R   Device ID */
+#define THRESH_TAP	0x1D	/* R/W Tap threshold */
+#define OFSX		0x1E	/* R/W X-axis offset */
+#define OFSY		0x1F	/* R/W Y-axis offset */
+#define OFSZ		0x20	/* R/W Z-axis offset */
+#define DUR		0x21	/* R/W Tap duration */
+#define LATENT		0x22	/* R/W Tap latency */
+#define WINDOW		0x23	/* R/W Tap window */
+#define THRESH_ACT	0x24	/* R/W Activity threshold */
+#define THRESH_INACT	0x25	/* R/W Inactivity threshold */
+#define TIME_INACT	0x26	/* R/W Inactivity time */
+#define ACT_INACT_CTL	0x27	/* R/W Axis enable control for activity and */
+/* inactivity detection */
+#define THRESH_FF	0x28	/* R/W Free-fall threshold */
+#define TIME_FF		0x29	/* R/W Free-fall time */
+#define TAP_AXES	0x2A	/* R/W Axis control for tap/double tap */
+#define ACT_TAP_STATUS	0x2B	/* R   Source of tap/double tap */
+#define BW_RATE		0x2C	/* R/W Data rate and power mode control */
+#define POWER_CTL	0x2D	/* R/W Power saving features control */
+#define INT_ENABLE	0x2E	/* R/W Interrupt enable control */
+#define INT_MAP		0x2F	/* R/W Interrupt mapping control */
+#define INT_SOURCE	0x30	/* R   Source of interrupts */
+#define DATA_FORMAT	0x31	/* R/W Data format control */
+#define DATAX0		0x32	/* R   X-Axis Data 0 */
+#define DATAX1		0x33	/* R   X-Axis Data 1 */
+#define DATAY0		0x34	/* R   Y-Axis Data 0 */
+#define DATAY1		0x35	/* R   Y-Axis Data 1 */
+#define DATAZ0		0x36	/* R   Z-Axis Data 0 */
+#define DATAZ1		0x37	/* R   Z-Axis Data 1 */
+#define FIFO_CTL	0x38	/* R/W FIFO control */
+#define FIFO_STATUS	0x39	/* R   FIFO status */
+#define TAP_SIGN	0x3A	/* R   Sign and source for tap/double tap */
 
 
 void pin_debug(uint8_t value);
@@ -244,6 +279,117 @@ void pwm_stop(void)
 	timer1_pwm_stop();
 }
 
+int8_t accel_init(void)
+{
+	uint8_t send[2];
+	twi_report_t* twi_report;
+	
+	send[0] = BW_RATE;
+	send[1] = 0x0A; // normal power, 100hz output rate (bw 50hz)
+	twi_write(ADXL345_ADDRESS, send, sizeof(send), NULL);
+	twi_report = twi_wait();
+	if(twi_report->error != 0)
+	{
+		return -1;
+	}
+	
+	send[0] = POWER_CTL;
+	send[1] = 0x08; // no auto sleep, measurement mode
+	twi_write(ADXL345_ADDRESS, send, sizeof(send), NULL);
+	twi_report = twi_wait();
+	if(twi_report->error != 0)
+	{
+		return -2;
+	}
+	
+	send[0] = DATA_FORMAT;
+	send[1] = 0x00; // +-2g, right justified, normal resolution (10bit)
+	twi_write(ADXL345_ADDRESS, send, sizeof(send), NULL);
+	twi_report = twi_wait();
+	if(twi_report->error != 0)
+	{
+		return -3;
+	}
+	
+	send[0] = FIFO_CTL;
+	send[1] = 0x00; // fifo bypass
+	twi_write(ADXL345_ADDRESS, send, sizeof(send), NULL);
+	twi_report = twi_wait();
+	if(twi_report->error != 0)
+	{
+		return -4;
+	}
+	
+	send[0] = OFSX;
+	send[1] = 0x00; // zero x-offset
+	twi_write(ADXL345_ADDRESS, send, sizeof(send), NULL);
+	twi_report = twi_wait();
+	if(twi_report->error != 0)
+	{
+		return -5;
+	}
+	
+	send[0] = OFSY;
+	send[1] = 0x00; // zero y-offset
+	twi_write(ADXL345_ADDRESS, send, sizeof(send), NULL);
+	twi_report = twi_wait();
+	if(twi_report->error != 0)
+	{
+		return -6;
+	}
+	
+	send[0] = OFSZ;
+	send[1] = 0x00; // zero z-offset
+	twi_write(ADXL345_ADDRESS, send, sizeof(send), NULL);
+	twi_report = twi_wait();
+	if(twi_report->error != 0)
+	{
+		return -7;
+	}
+	
+	send[0] = INT_ENABLE;
+	send[1] = 0x00; // zero y-offset
+	twi_write(ADXL345_ADDRESS, send, sizeof(send), NULL);
+	twi_report = twi_wait();
+	if(twi_report->error != 0)
+	{
+		return -8;
+	}
+	
+	return 0;
+}
+
+int8_t accel_x(uint16_t* value)
+{
+	twi_report_t* twi_report;
+	uint8_t send[1];
+	
+	send[0] = DATAX0;
+	twi_write(ADXL345_ADDRESS, send, sizeof(send), NULL);
+	twi_report = twi_wait();
+	if(twi_report->error != 0)
+	{
+		return -1;
+	}
+	
+	
+	twi_read(ADXL345_ADDRESS, 2, NULL);
+	twi_report = twi_wait();
+	if(twi_report->error != 0)
+	{
+		return -2;
+	}
+	
+	if(twi_report->length != 2)
+	{
+		return -3;
+	}
+	
+	*value = ((((uint16_t)twi_report->data[1])<<8) | ((uint16_t)twi_report->data[0])) & 0x03FF;
+	
+	return 0;
+}
+
 
 #ifdef ACCEL_TEST
 
@@ -251,14 +397,26 @@ int main(void)
 {
 	uint8_t send[] = {0x01,0x02,0x03};
 	twi_report_t* twi_report;
+	uint16_t value;
 
 	DDRB |= (1<<DDB0);
 	PORTB &= ~(1<<DDB0);
-
+	
+	twi_init();
+	wait_1ms(1);
+	
+	accel_init();
+	wait_1ms(10);
+	
 
 	while(1)
 	{
+		accel_x(&value);
+		PORTB |= (1<<DDB0);
+		wait_1ms(value);
+		PORTB &= ~(1<<DDB0);
 		
+		wait_1ms(1000);
 	}
 }
 
@@ -279,7 +437,7 @@ int main(void)
 	DDRB |= (1<<DDB2); //OC1B
 
 
-	DDRB |= (1<<DDB0);
+	DDRB |= (1<<DDB0); //DEBUG PIN
 	PORTB &= ~(1<<DDB0);
 	
 	pwm_init();
